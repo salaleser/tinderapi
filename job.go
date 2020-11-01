73df7532ed76a9ae65688150914428a36e9a1f7b
salaleser <salaleser@users.noreply.github.com>
2020-11-01 07:24:12 +0300

    Fix struct

--- job.go
+++ job.go
@@ -3,7 +3,7 @@ package tinderapi
 type Job struct {
 	Company struct {
 		Name      string `json:"name,omitempty"`
-		Displayed bool   `json:"displayed",omitempty`
+		Displayed bool   `json:"displayed,omitempty"`
 	} `json:"company,omitempty"`
 	Title struct {
 		Displayed bool   `json:"displayed,omitempty"`
