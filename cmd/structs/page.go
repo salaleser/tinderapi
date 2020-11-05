d266ba50873f05cd919e73a77c66b3a2419f7075
salaleser <salaleser@users.noreply.github.com>
2020-11-06 01:30:44 +0300

    Refactor

--- cmd/structs/page.go
+++ cmd/structs/page.go
@@ -1,11 +1,11 @@
 package structs
 
 type Page struct {
-	Meta        Meta  `json:"meta,omitempty"`
-	Data        Data  `json:"data,omitempty"`
-	Error       Error `json:"error,omitempty"`
-	BatchErrors []struct {
-		Error Error  `json:"error,omitempty"`
-		Key   string `json:"key,omitempty"`
+	Meta        *Meta  `json:"meta,omitempty"`
+	Data        *Data  `json:"data,omitempty"`
+	Error       *Error `json:"error,omitempty"`
+	BatchErrors *[]struct {
+		Error *Error  `json:"error,omitempty"`
+		Key   *string `json:"key,omitempty"`
 	} `json:"batch_errors,omitempty"`
 }
