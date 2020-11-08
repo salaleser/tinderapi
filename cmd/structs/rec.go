4ba3bba03777bdf6a2bb7bb82bb1af97b84975f7
salaleser <salaleser@users.noreply.github.com>
2020-11-08 17:03:09 +0300

    Add fields to Rec

--- cmd/structs/rec.go
+++ cmd/structs/rec.go
@@ -1,10 +1,23 @@
 package structs
 
 type Rec struct {
-	Type      string    `json:"type,omitempty"`
-	User      User      `json:"user,omitempty"`
-	Facebook  Facebook  `json:"facebook,omitempty"`
-	Spotify   Spotify   `json:"spotify,omitempty"`
-	Teaser    Teaser    `json:"teaser,omitempty"`
-	Instagram Instagram `json:"instagram,omitempty"`
+	Type           string    `json:"type,omitempty"`
+	User           User      `json:"user,omitempty"`
+	Facebook       Facebook  `json:"facebook,omitempty"`
+	Spotify        Spotify   `json:"spotify,omitempty"`
+	Teaser         Teaser    `json:"teaser,omitempty"`
+	Instagram      Instagram `json:"instagram,omitempty"`
+	DistanceMi     int       `json:"distance_mi,omitempty"`
+	ContentHash    string    `json:"content_hash,omitempty"`
+	SNumber        int       `json:"s_number,omitempty"`
+	Teasers        []Teaser  `json:"teasers,omitempty"`
+	ExperimentInfo struct {
+		UserInterests struct {
+			SelectedInterests []struct {
+				ID   string `json:"id,omitempty"`
+				Name string `json:"name,omitempty"`
+			} `json:"selected_interests,omitempty"`
+		} `json:"user_interests,omitempty"`
+	} `json:"experiment_info,omitempty"`
+	IsSuperlikeUpsell bool `json:"is_superlike_upsell,omitempty"`
 }
